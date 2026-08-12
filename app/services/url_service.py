187e42b0dc7e9572058dc6bea2fdc7497e2e0ff1
Subham Parajuli <suvamparajuli222@gmail.com>
2026-08-12 22:17:08 +0545

    added redis cache in the system

--- app/services/url_service.py
+++ app/services/url_service.py
@@ -3,7 +3,9 @@ import secrets
 
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
-
+from app.redis_client import redis_client
+from app.cache_metrics import cache_metrics
+from redis.exceptions import RedisError
 from app.repositories.url_repo import (
     create_url,
     get_url_by_short_code,
@@ -37,3 +39,57 @@ def shorten_url(
     raise RuntimeError(
         "Unable to generate a unique short code."
     )
+
+def get_original_url(
+    db: Session,
+    short_code: str,
+):
+    cache_key=f"url:{short_code}"
+
+    try:
+        cached_url=redis_client.get(cache_key)
+
+        if cached_url:
+            cache_metrics.hits += 1
+            return cached_url
+        cache_metrics.misses +=1
+    except RedisError:
+        pass
+    
+    url= get_url_by_short_code(
+        db=db,
+        short_code=short_code,
+    )
+    if url is None:
+        return None
+    try:
+        redis_client.set(
+            cache_key,
+            url.original_url,ex=3000
+        )
+    except RedisError:
+        cache_metrics.misses += 1
+        
+    return url.original_url
+
+
+def delete_short_url(db:Session,short_code:str)->bool:
+    url=get_url_by_short_code(
+        db=db,
+        short_code=short_code
+    )
+    if url is None:
+        return False 
+
+    delete_url(
+        db=db,
+        short_code=url
+    )
+    try:
+        redis_client.delete(
+            f"url:{short_code}"
+        )
+    except RedisError:
+        pass
+
+    return True
