9f7ea810a49c0fe22c810c6cea324721d12b1569
Subham Parajuli <suvamparajuli222@gmail.com>
2026-08-12 22:15:52 +0545

    added all router next:add redis cacahe in the system :

--- app/routers/urls.py
+++ app/routers/urls.py
@@ -21,3 +21,35 @@ def create_short_url(request:URLCreate,db:Session=Depends(get_db)):
         short_code=url.short_code,
         short_url=f"http://localhost:8000/{url.short_code}"
     )
+
+@router.get("/{short_code}")
+def redirect_to_original(short_code:str,db:Session=Depends(get_db)):
+    original_url=get_original_url(
+        db=db,
+        short_code=short_code
+    )
+    if original_url is None:
+        raise HTTPException(
+            status_code=404,
+            detail="URL not found"
+        )
+    return RedirectResponse(
+        url=original_url,
+        status_code=302
+    )
+
+
+@router.delete("/{short_code}")
+def delete_short_url_endpoint(
+    short_code:str,
+    db:Session=Depends(get_db)
+):
+    deleted=delete_short_url(db=db,short_code=short_code)
+    if not deleted:
+        raise HTTPException(
+            status_code=404,
+            detail="URL not found"
+        )
+    return{
+        "message":"URL deleted"
+    }
